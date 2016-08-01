4360f5a4ec2b28ded34c3d3697c0af17b4a8c3cf
asherkory <asher.kory@gmail.com>
2016-08-01 13:06:09 -0400

    Complete lesson

--- password.js
+++ password.js
@@ -1,1 +1,18 @@
-// code solution here
+var length = parseInt(process.argv[2], 10);
+var quantity = parseInt(process.argv[3], 10);
+
+function generatePassword (passLength) {
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz1234567890!@#$%^&*";
+  var password = "";
+  for (var i = 0; i < passLength; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+  return password;
+}
+
+for (var i = 0; i < quantity; i++) {
+  console.log(generatePassword(length));
+}
+
+process.exit(0);
