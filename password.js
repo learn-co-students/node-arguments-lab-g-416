e70b905074cce2b3c279967834d10f8fc4cdb04e
davdkm <davd.km@gmail.com>
2016-09-14 20:34:37 -0700

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,34 @@
 // code solution here
+var password = '';
+var length = parseInt(process.argv[2]);
+var quantity = parseInt(process.argv[3]);
+var remainder = length % 16;
+var x = Math.floor(length / 16);
+
+var randomString =  function () {
+  return Math.random().toString(36).slice(-16);
+}
+
+var generatePassword = function () {
+  if (x <= 1) {
+    return randomString().slice(-length);
+  } else {
+    password = '';
+    for (var i = 0; i < x; i++) {
+      password += randomString();
+    }
+    return password += randomString().slice(-remainder)
+  }
+}
+
+if (!process.argv[3]) {
+  console.log('Missing arguments');
+} else if ( isNaN(length)|| isNaN(quantity) ) {
+  console.log('Arguments must be numbers');
+} else {
+  for (var i = 0; i < quantity; i++) {
+    console.log(generatePassword());
+  }
+}
+
+process.exit(0);
