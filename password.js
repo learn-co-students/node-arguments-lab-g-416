f4f48902b6b0674b3bbcdce97de32d71e943b7fe
hudbuz <hzb2@cornell.edu>
2016-11-04 16:01:25 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,16 @@
-// code solution here
+var passwords = []
+process.argv.shift()// code solution here
+process.argv.shift()// code solution here
+
+var password = function() {
+  for (i = 0; i < process.argv[1]; i ++) {
+    passwords.push(Math.round(Math.random(0,1) * Math.pow(10, process.argv[0])).toString(10.1))
+
+
+  }
+for (i = 0; i < passwords.length; i ++) {
+  console.log(passwords[i])
+}
+}
+
+password()
