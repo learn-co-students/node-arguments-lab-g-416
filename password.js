1b5746e6e65e792dd26ed258da60d4885986e637
wedesignstudios <deelden@gmail.com>
2017-01-04 08:52:31 -0600

    Done

--- password.js
+++ password.js
@@ -1,1 +1,19 @@
-// code solution here
+function generatePassword(pwdLength) {
+    var length = pwdLength,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; i++) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(retVal);    
+}
+
+for (var i = 0; i < process.argv[3]; i++) {
+  generatePassword(process.argv[2]);
+}
+
+if (process.argv.length < 4) {
+  console.log("Wrong number of arguments. Try again.")
+}
+
+process.exit(0);
