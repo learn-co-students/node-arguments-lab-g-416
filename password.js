aa7917c3f4c942dcd91c5664c0e9919477d66c16
kaileighrose <kaileighrose@gmail.com>
2017-01-08 23:57:36 -0500

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,21 @@
-// code solution here
+function generatePassword(vlength) {
+    var length = vlength,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+
+process.argv.shift()
+process.argv.shift()
+var plength = process.argv[0]
+var num = process.argv[1]
+while (num > 0) {
+  console.log(generatePassword(plength))
+  num--
+}
+
+process.exit(0)
