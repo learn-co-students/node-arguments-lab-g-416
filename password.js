9281ad483c2740df08b82e18b1b4fbeb8adb1cc0
Jason <jasonkwong11@gmail.com>
2017-02-01 22:50:35 -0500

    done

--- password.js
+++ password.js
@@ -1,1 +1,28 @@
-// code solution here
+const randomNum = () => {
+  min = Math.ceil(1);
+  max = Math.floor(9);
+  return (Math.floor(Math.random() * (max - min)) + min).toString();
+}
+
+const generatePassword = (passwordLength) => {
+  var passwordLength = passwordLength;
+  var password = "";
+  for (var i=0; i < passwordLength; i++){
+    password += randomNum();
+  }
+  return password;
+}
+
+process.argv.shift()
+process.argv.shift()
+
+const createPasswords = (passwordLength, numberOfPasswords) => {
+  var passwords = Object.assign([]);
+
+  for (i=0; i < numberOfPasswords; i++){
+    console.log(generatePassword(passwordLength));
+  }
+}
+
+createPasswords(process.argv[0], process.argv[1]);
+process.exit(0)
