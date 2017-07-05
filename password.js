a22b6be8bdd660d7042d6bc766426858b0d2bdf5
Mark Stein <mystycs@gmail.com>
2017-07-05 06:57:27 +0000

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,13 @@
-// code solution here
+var length = process.argv[2];
+var quantity = process.argv[3];
+
+for (var i = 0; i < quantity; i++) {
+  var password = "";
+  var keyValues = "abcdefghijklmnopqrstvuwxyzABCDEFGHIJKLMNOPQRSTVUWXYZ0123456789";
+  for (var j = 0; j < length; j++) {
+    password += keyValues[Math.floor(Math.random() * keyValues.length)];
+  }
+  console.log(password);
+}
+
+process.exit(0);
