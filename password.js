b70b611d50f393e7b024737d39d23fb5ea227abf
Michael Ries <michael.ries@gmail.com>
2017-08-22 14:36:43 -0500

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,15 @@
 // code solution here
+length = process.argv[2];
+number = process.argv[3];
+
+const randomPassword = length => {
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += String.fromCharCode(Math.random()*(122-97+1) + 97);
+  }
+  return password;
+}
+
+for(var i = 0; i < number; i++) {
+  console.log(randomPassword(length));
+}
