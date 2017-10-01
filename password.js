71f5e444a6ea494defc6f3e5c98c249f5989aed8
Yechiel Kalmenson <contact@yechiel.me>
2017-10-01 12:21:23 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,25 @@
 // code solution here
+var length = process.argv[2]
+var amount = process.argv[3]
+
+
+generatePasswords(length, amount)
+
+function generatePasswords(length, amount) {
+
+    var passwords = []
+
+    for (i = 0; i < amount; i++ ) {
+        var password = ""
+        for (j = 0; j < length; j++ ) {
+            password += Math.floor((Math.random() * 10))
+        }
+        passwords.push(password)
+    }
+
+    passwords.forEach(function(password) {
+        console.log(password)
+    })
+}
+
+process.exit()
