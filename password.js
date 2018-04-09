26136eb590886084fa5ccf361d004b983edd36f5
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-09 13:53:18 +0200

    Done.

--- password.js
+++ password.js
@@ -1,30 +1,21 @@
-var generatePassword = function generatePassword(length, charset) {
-  var length = length || 8,
-    charset = charset || 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-    password = '';
-  for (var i = 0, n = charset.length; i < length; ++i) {
-    password += charset.charAt(Math.floor(Math.random() * n))
+function generatePassword(length) {
+  let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let l = parseInt(length, 10);
+  let retVal = '';
+
+  for (let i = 0, n = charset.length; i < l; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
   }
-  return password
+  console.log(retVal);
+  return retVal;
 }
 
-var length = 10,
-  number = 1
-var argv = process.argv
-
-if (!argv[2] || !argv[3]) {
-  console.log('Please provide an argument, e.g., 5 or 5 10')
-  process.exit(1)
-}
-if (argv[2]) {
-  length = parseInt(argv[2], 10)
-}
-if (argv[3]) {
-  number = parseInt(argv[3], 10)
+function createPassword(length, number) {
+  let i = 0;
+  while (i < number) {
+    generatePassword(length);
+    i++;
+  }
 }
 
-for (var i = 1; i <=number; i++) {
-  console.log(generatePassword(length))
-}
-
-process.exit(0)
+module.exports = createPassword;
