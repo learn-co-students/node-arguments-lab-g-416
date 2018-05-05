5f1f0172e8ef2751a0b68795900b0f56a50ebaad
github username <dsbondin@gmail.com>
2018-05-05 14:43:44 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,15 @@
 // code solution here
+const length = process.argv[2];
+const count = process.argv[3];
+
+function generatePassword(passwordLength) {
+  let password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    password += String.fromCharCode(Math.floor(Math.random() * 26) + 65 + Math.round(Math.random()) * 32)
+  }
+  return password;
+}
+
+for (let i = 0; i < count; i ++) {
+  console.log(generatePassword(length));
+}
