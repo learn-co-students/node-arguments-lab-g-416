0e94ce6155fc4a7b6de342c948d3a680cc336eef
github username <juliannne@gmail.com>
2018-05-14 12:24:51 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,17 @@
 // code solution here
+length = process.argv[2];
+numOfPasswords = process.argv[3];
+
+function generatePassword(length) {
+  let password = "";
+  let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (let i = 0; i < length; i++) {
+    let character = Math.floor(Math.random() * chars.length);
+    password += chars.charAt(character)
+  }
+  return password
+}
+
+for (let i = 0; i < numOfPasswords; i++) {
+  console.log(generatePassword(length));
+}
