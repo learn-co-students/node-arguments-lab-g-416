7083ae4db45bd21c0c4ed176ab2c8dc97cba994d
cdhenry <cdhenry@gmail.com>
2018-07-08 16:37:17 -0400

    Done.

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 process.argv.shift()
 process.argv.shift()
-length = parseInt(process.argv[0])
-numOfPasswords = parseInt(process.argv[1])
+length = parseInt(process.argv[0], 10)
+numOfPasswords = parseInt(process.argv[1], 10)
 
 while (numOfPasswords > 0){
   console.log(generatePassword(length))
@@ -10,6 +10,7 @@ while (numOfPasswords > 0){
 
 function generatePassword (length) {
 	var password = '', character;
+  debugger;
 	while (length > password.length) {
 		if (password.indexOf(character = String.fromCharCode(Math.floor(Math.random() * 94) + 33), Math.floor(password.length / 94) * 94) < 0) {
 			password += character;
