72e18916d6ffc246811b4ffbc3c1f4c23b6df3c9
github username <github email address>
2018-09-26 17:49:13 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,16 @@
 // code solution here
+function generatePassword(passwordLength) {
+  const length = passwordLength;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  console.log(retVal)
+  return retVal;
+}
+
+const passwordLength = parseInt(process.argv[process.argv.length - 2])
+for (var i = 0; i < parseInt(process.argv[process.argv.length - 1]); i++) {
+  generatePassword(passwordLength)
+}
