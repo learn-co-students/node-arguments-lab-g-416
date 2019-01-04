7c2f750076e410220391ae193e8518f3ca8c9db3
yutingcxiang <yutingcxiang@gmail.com>
2019-01-04 14:53:17 -0500

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,27 @@
-// code solution here
+var generatePassword = function generatePassword(length) {
+  var length = length,
+    charset = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+    password = '';
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n))
+  }
+  return password
+}
+
+
+if (!process.argv[2] || !process.argv[3]) {
+  console.log('Error. Missing argument.')
+  process.exit(0)
+}
+if (process.argv[2]) {
+  length = parseInt(process.argv[2], 10)
+}
+if (process.argv[3]) {
+  number = parseInt(process.argv[3], 10)
+}
+
+for (var i = 1; i <=number; i++) {
+  console.log(generatePassword(length))
+}
+
+process.exit(0)
