d700aa02e3dc87c7e3943ae07a21b1bc07992beb
rockychiang <rocky@chem.ucla.edu>
2019-04-15 23:00:19 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,17 @@
-// code solution here
+const generatePassword = (length) => {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text
+}
+
+let number = process.argv.pop()
+let length = process.argv.pop()
+
+for (let i = 0; i < number; i++) {
+  console.log(generatePassword(length))
+}
+
+process.exit(0)
