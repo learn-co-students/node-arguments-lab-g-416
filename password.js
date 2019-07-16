962529e2aae36b6a9c7b13b575154b208e786db4
Kevin Huang <yellow.khaos@gmail.com>
2019-07-16 22:21:19 +0000

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,25 @@
-// code solution here
+function generatePassword(length) {
+  let password = ""
+  for (i=0; i < length; i++) {
+    password = password + `${Math.floor(Math.random()*10)}`
+  }
+  return password
+}
+
+function password(number, length) {
+
+  for (i=0; i< number; i++) {
+    console.log(generatePassword(length))
+  }
+  process.exit(0)
+}
+
+process.argv.shift()
+process.argv.shift()
+if(process.argv.length !== 2){
+  console.log("The number of arguments is incorrect")
+} else {
+  let number = process.argv.shift()
+  let length = process.argv.shift()
+  module.exports = password(length, number)
+}
