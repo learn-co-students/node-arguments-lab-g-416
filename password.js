711074f481303e25bc24225862d2ad68bf1c956f
treuther <tracyreuther@gmail.com>
2020-07-31 04:00:17 -0500

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,14 @@
 // code solution here
+process.argv.shift()
+process.argv.shift()
+
+let length = process.argv[0]
+charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" //global variable
+reVal = ""
+
+for (let i = 0, n = charset.length; i < length; ++i) {
+    retval += charset.charAt(Math.floor(Math.random() * n));
+}
+
+console.log(retVal)
+prociess.exit(0)
