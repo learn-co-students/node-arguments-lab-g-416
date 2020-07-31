e5c90c63e156b7eff900012d19b1aa57992405f9
treuther <tracyreuther@gmail.com>
2020-07-31 04:02:36 -0500

    Done.

--- password.js
+++ password.js
@@ -2,13 +2,13 @@
 process.argv.shift()
 process.argv.shift()
 
-let length = process.argv[0]
-charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" //global variable
-reVal = ""
+let length = process.argv[0],
+charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+retVal = "";
 
 for (let i = 0, n = charset.length; i < length; ++i) {
-    retval += charset.charAt(Math.floor(Math.random() * n));
+    retVal += charset.charAt(Math.floor(Math.random() * n));
 }
 
 console.log(retVal)
-prociess.exit(0)
+process.exit(0)
