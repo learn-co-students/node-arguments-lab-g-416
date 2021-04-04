03b50f358db292d7f5da2d9561dd363b2b778c2d
Dasha Lary <dasha4988@gmail.com>
2021-04-04 18:40:24 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,16 @@
-// code solution here
+let passwordLength = process.argv[2];
+let quantity = process.argv[3];
+
+const generatePassword = function(passwordLength, quantity) {
+  const options = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*abcdefghijklmnopqrstuvwxyz";
+  let text = '';
+
+  for (let i = 1; i <= passwordLength; i++) {
+      text += options.charAt([Math.floor(Math.random() * options.length)]);
+    }
+  return text;
+}
+
+console.log(generatePassword(passwordLength, quantity));
+
+process.exit(0);
